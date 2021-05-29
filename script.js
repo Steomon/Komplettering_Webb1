717eae80a1df62c11e4c9d149a09aa1d9780847b
Steomon <38396746+Steomon@users.noreply.github.com>
2021-05-29 23:27:11 +0200

    Try to get id from checkbox

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-var checkboxValues = JSON.parse(localStorage.getItem('checkboxValues')) || {},
-    $checkboxes = $(".checkboxes :checkbox");
+var test = document.getElementById("checkbox1").value;
+console.log(test);
 
-$checkboxes.on("change", function(){
-  $checkboxes.each(function(){
-    checkboxValues[this.id] = this.checked;
-  });
-  
-  localStorage.setItem("checkboxValues", JSON.stringify(checkboxValues));
-});
-
-$.each(checkboxValues, function(key, value) {
-  $("#" + key).prop('checked', value);
-});
 
