e0d19e972f9487b7f5151a512dea5e78eb78aa08
Steomon <38396746+Steomon@users.noreply.github.com>
2021-05-30 00:16:15 +0200

    test

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
+/*function uncheck() {
+    document.getElementsByClassName("checkboxes :checkbox").checked = false;
+}
+
+function uncheckAll() {
+    document.querySelectorAll('input[type="checkbox"]')
+      .forEach(el => el.checked = false);
+  }
+  
+  document.querySelector('button').addEventListener('click', uncheckAll)
+
+  Jag försökte hitta ett sätt att tömma alla checkboxes, men det fungerade inte 
 
 
+  Jag försökte även spara valuen av checkboxes och spara dem i localstorage för att 
+  checkboxes inte ska reset:as när man laddar om sidan, men det fungerade inte heller
+  */
